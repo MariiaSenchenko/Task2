fc5e57a56b44920c1112242f79df1fffc3f7a18b
MariiaSenchenko <61646046+MariiaSenchenko@users.noreply.github.com>
2020-03-07 10:02:32 +0200

    commit 45

--- src/com/it/task2.java
+++ src/com/it/task2.java
@@ -2,6 +2,13 @@ package com.it;
 
 public class task2 {
     public static void main(String[] args){
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+
 
     }
 }
